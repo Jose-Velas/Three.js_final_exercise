2264b6fb8dcad4e93cab12e7792db07bc705e35d
Jose-Velas <110056510+Jose-Velas@users.noreply.github.com>
2023-01-25 21:20:25 +0100

    Format document with Prettier

--- viewer.js
+++ viewer.js
@@ -1,58 +1,63 @@
 import {
-    Scene,
-    BoxGeometry,
-    MeshBasicMaterial,
-    Mesh,
-    PerspectiveCamera,
-    WebGLRenderer,
-    Vector2,
-    Vector3,
-    Vector4,
-    Quaternion,
-    Matrix4,
-    Spherical,
-    Box3,
-    Sphere,
-    Raycaster,
-    MathUtils,
-    MOUSE,
-    Clock,
-    AmbientLight,
-    DirectionalLight,
-    MeshPhongMaterial,
-    AxesHelper,
-    GridHelper
-} from 'three';
+  Scene,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+  PerspectiveCamera,
+  WebGLRenderer,
+  Vector2,
+  Vector3,
+  Vector4,
+  Quaternion,
+  Matrix4,
+  Spherical,
+  Box3,
+  Sphere,
+  Raycaster,
+  MathUtils,
+  MOUSE,
+  Clock,
+  AmbientLight,
+  DirectionalLight,
+  MeshPhongMaterial,
+  AxesHelper,
+  GridHelper,
+} from "three";
 
-import CameraControls from 'camera-controls';
+import CameraControls from "camera-controls";
 
 const subsetOfTHREE = {
-    MOUSE,
-    Vector2,
-    Vector3,
-    Vector4,
-    Quaternion,
-    Matrix4,
-    Spherical,
-    Box3,
-    Sphere,
-    Raycaster,
-    MathUtils: {
-      DEG2RAD: MathUtils.DEG2RAD,
-      clamp: MathUtils.clamp
-    }
+  MOUSE,
+  Vector2,
+  Vector3,
+  Vector4,
+  Quaternion,
+  Matrix4,
+  Spherical,
+  Box3,
+  Sphere,
+  Raycaster,
+  MathUtils: {
+    DEG2RAD: MathUtils.DEG2RAD,
+    clamp: MathUtils.clamp,
+  },
 };
 
-const canvas = document.getElementById('three-canvas');
+const canvas = document.getElementById("three-canvas");
 
 // 1 Scene
 const scene = new Scene();
 
 // 2 The object
 const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshPhongMaterial({color: 0x8025E0, specular: 'white', shininess: 100, flatShading: true});
+const material = new MeshPhongMaterial({
+  color: 0x8025e0,
+  specular: "white",
+  shininess: 100,
+  flatShading: true,
+});
 const orangeCube = new Mesh(geometry, material);
-orangeCube.position.set(1,0.5,0);
+orangeCube.position.set(1, 0.5, 0);
 scene.add(orangeCube);
 
 const axes = new AxesHelper();
@@ -60,18 +65,21 @@ axes.material.depthTest = false;
 axes.renderOrder = 3;
 scene.add(axes);
 
-const grid = new GridHelper(20,20);
+const grid = new GridHelper(20, 20);
 grid.renderOrder = 2;
 scene.add(grid);
 
 // 3 The camera
-const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight);
-camera.position.set(5,4,2);
+const camera = new PerspectiveCamera(
+  75,
+  canvas.clientWidth / canvas.clientHeight
+);
+camera.position.set(5, 4, 2);
 scene.add(camera);
 
 // 4 The renderer
 const renderer = new WebGLRenderer({
-    canvas: canvas,
+  canvas: canvas,
 });
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
@@ -84,71 +92,71 @@ const ambientLight = new AmbientLight(color, intensity);
 scene.add(ambientLight);
 
 const directionalLight = new DirectionalLight(color, intensity);
-directionalLight.position.set(-1,2,-1);
+directionalLight.position.set(-1, 2, -1);
 scene.add(directionalLight);
 
 // 6 Resposivity
-window.addEventListener('resize', () => {
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-})
+window.addEventListener("resize", () => {
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+});
 
 // 7 Controls
-CameraControls.install( { THREE: subsetOfTHREE } ); 
+CameraControls.install({ THREE: subsetOfTHREE });
 const clock = new Clock();
 const cameraControls = new CameraControls(camera, canvas);
 
 function animate() {
   const delta = clock.getDelta();
-	cameraControls.update( delta );
-	renderer.render( scene, camera );
+  cameraControls.update(delta);
+  renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
 
 animate();
 
-function verHerrA () {
-    var estiloA = document.getElementById("btn1"); 
-    estiloA.className = "btnherrpress";
+function verHerrA() {
+  var estiloA = document.getElementById("btn1");
+  estiloA.className = "btnherrpress";
 
-    var mostrarbar2 = document.getElementById("toolbar2");
-    mostrarbar2.className = "vflex";
+  var mostrarbar2 = document.getElementById("toolbar2");
+  mostrarbar2.className = "vflex";
 
-    var mostrarTools1 = document.getElementById("btn1tools");
-    mostrarTools1.className = "vsbl";
+  var mostrarTools1 = document.getElementById("btn1tools");
+  mostrarTools1.className = "vsbl";
 
-    var ocultarB = document.getElementById("btn2");
-    ocultarB.className = "oclt";
+  var ocultarB = document.getElementById("btn2");
+  ocultarB.className = "oclt";
 }
 
-function verHerrB () {
-    var estiloB = document.getElementById("btn2"); 
-    estiloB.className = "btnherrpress";
+function verHerrB() {
+  var estiloB = document.getElementById("btn2");
+  estiloB.className = "btnherrpress";
 
-    var mostrarbar2 = document.getElementById("toolbar2");
-    mostrarbar2.className = "vflex";
+  var mostrarbar2 = document.getElementById("toolbar2");
+  mostrarbar2.className = "vflex";
 
-    var mostrarTools2 = document.getElementById("btn2tools");
-    mostrarTools2.className = "vsbl";
+  var mostrarTools2 = document.getElementById("btn2tools");
+  mostrarTools2.className = "vsbl";
 
-    var ocultarA = document.getElementById("btn1");
-    ocultarA.className = "oclt";
+  var ocultarA = document.getElementById("btn1");
+  ocultarA.className = "oclt";
 }
 
-function verHerrR () {
-    var estiloA = document.getElementById("btn1"); 
-    estiloA.className = "btnherr";
-    var estiloB = document.getElementById("btn2"); 
-    estiloB.className = "btnherr";
+function verHerrR() {
+  var estiloA = document.getElementById("btn1");
+  estiloA.className = "btnherr";
+  var estiloB = document.getElementById("btn2");
+  estiloB.className = "btnherr";
 
-    var mostrarbar2 = document.getElementById("toolbar2");
-    mostrarbar2.className = "oclt";
+  var mostrarbar2 = document.getElementById("toolbar2");
+  mostrarbar2.className = "oclt";
 
-    var mostrarTools1 = document.getElementById("btn1tools");
-    mostrarTools1.className = "oclt";
-    var mostrarTools2 = document.getElementById("btn2tools");
-    mostrarTools2.className = "oclt";
+  var mostrarTools1 = document.getElementById("btn1tools");
+  mostrarTools1.className = "oclt";
+  var mostrarTools2 = document.getElementById("btn2tools");
+  mostrarTools2.className = "oclt";
 }
 
 document.getElementById("btn1").onclick = verHerrA;
