ef3fcfff705140dcedcb199844d5a12d8784849b
Jose-Velas <110056510+Jose-Velas@users.noreply.github.com>
2023-01-25 20:43:43 +0100

    Delete the iframe code and add Three.js and CameraControls.js

--- viewer.js
+++ viewer.js
@@ -1,14 +1,112 @@
-import { projects } from "./projects.js";
+import {
+    Scene,
+    BoxGeometry,
+    MeshBasicMaterial,
+    Mesh,
+    PerspectiveCamera,
+    WebGLRenderer,
+    Vector2,
+    Vector3,
+    Vector4,
+    Quaternion,
+    Matrix4,
+    Spherical,
+    Box3,
+    Sphere,
+    Raycaster,
+    MathUtils,
+    MOUSE,
+    Clock,
+    AmbientLight,
+    DirectionalLight,
+    MeshPhongMaterial,
+    AxesHelper,
+    GridHelper
+} from 'three';
 
-const currentURL = window.location.href;
-const url = new URL(currentURL);
-const currentProjectID = url.searchParams.get("id");
+import CameraControls from 'camera-controls';
 
-const currentProject = projects.find(projects => projects.id === currentProjectID);
+const subsetOfTHREE = {
+    MOUSE,
+    Vector2,
+    Vector3,
+    Vector4,
+    Quaternion,
+    Matrix4,
+    Spherical,
+    Box3,
+    Sphere,
+    Raycaster,
+    MathUtils: {
+      DEG2RAD: MathUtils.DEG2RAD,
+      clamp: MathUtils.clamp
+    }
+};
 
-const iframe = document.getElementById('model-iframe');
-iframe.src = currentProject.url
+const canvas = document.getElementById('three-canvas');
 
+// 1 Scene
+const scene = new Scene();
+
+// 2 The object
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshPhongMaterial({color: 0x8025E0, specular: 'white', shininess: 100, flatShading: true});
+const orangeCube = new Mesh(geometry, material);
+orangeCube.position.set(1,0.5,0);
+scene.add(orangeCube);
+
+const axes = new AxesHelper();
+axes.material.depthTest = false;
+axes.renderOrder = 3;
+scene.add(axes);
+
+const grid = new GridHelper(20,20);
+grid.renderOrder = 2;
+scene.add(grid);
+
+// 3 The camera
+const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight);
+camera.position.set(5,4,2);
+scene.add(camera);
+
+// 4 The renderer
+const renderer = new WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+renderer.setClearColor(0x282828, 1);
+
+// 5 Lights
+const color = 0xffffff;
+const intensity = 1;
+const ambientLight = new AmbientLight(color, intensity);
+scene.add(ambientLight);
+
+const directionalLight = new DirectionalLight(color, intensity);
+directionalLight.position.set(-1,2,-1);
+scene.add(directionalLight);
+
+// 6 Resposivity
+window.addEventListener('resize', () => {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+})
+
+// 7 Controls
+CameraControls.install( { THREE: subsetOfTHREE } ); 
+const clock = new Clock();
+const cameraControls = new CameraControls(camera, canvas);
+
+function animate() {
+  const delta = clock.getDelta();
+	cameraControls.update( delta );
+	renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+}
+
+animate();
 
 function verHerrA () {
     var estiloA = document.getElementById("btn1"); 
